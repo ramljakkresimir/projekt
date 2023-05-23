b639ce1afec43a948c69025e7c197f0fb1cb5224
ramljakkresimir <95712829+ramljakkresimir@users.noreply.github.com>
2023-05-23 22:36:28 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,64 +1,48 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "header.h"
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef struct korisnik
+{
+	char ime[20];
+	char prezime[20];
+	int idPutovanja;
+}KORISNIK;
 
 
-int izbornik(void) {
-	int korisnickiOdabir;
+typedef struct linije {
+	int idPutovanja;
+	char ime[64];
+	char drzava[64];
+	int brojSlobodnihMjesta;
+	float cijena;
+}LINIJE;
 
-	const char* imeDatLinije = "linije.bin";
-	const char* imeDatKupci = "kupci.bin";
+void unosLinija(const char* imeDat);
 
-	printf("=================================\n");
-	printf("|-------------------------------|\n");
-	printf("|      Autobusni kolodvor     |\n");
-	printf("|-------------------------------|\n");
-	printf("=================================\n\n");
-	printf("%d. Rezervacija karata\n", 1);
-	printf("%d. Ispis broja sjedala\n", 2);
-	printf("%d. Pretrazivanje\n", 3);
-	printf("%d. Odjava rezervacije\n", 4);
-	printf("%d. Ispis linija\n", 5);
-	printf("%d. Izlaz\n", 6);
-	printf("Vas odabir:  ");
-	do {
-		scanf("%d", &korisnickiOdabir);
-		if (1 > korisnickiOdabir || korisnickiOdabir > 6) {
-			printf("Opcija za Vas unos ne postoji.\n Novi unos: ");
-		}
-	} while (!(1 <= korisnickiOdabir && korisnickiOdabir <= 6));
 
-	switch (korisnickiOdabir) {
-	case 1:
-		rezIspis(imeDatLinije, imeDatKupci);
-		break;
-	case 2:
-		ispisBrojaSjedala(imeDatLinije);
-		break;
-	case 3: {
-		KORISNIK* trazeni = pretrazivanje(imeDatKupci);
-		if (trazeni == NULL) {
-			printf("\nTrazeni kupac se ne nalazi u datoteci.\n\n");
-		}
-		else {
-			printf("\n%s %s se nalazi u kupcima i putuje s linijom %d.\n\n", trazeni->ime, trazeni->prezime, trazeni->idPutovanja);
-		}
-		break;
-	}
-	case 4:
-		odjava(imeDatLinije, imeDatKupci);
-		break;
-	case 5:
-		ispisLinija(imeDatLinije);
-		break;
-	case 6:
-		izlaz();
-		break;
-	default:
-		break;
-	}
+int izbornik();
 
-	return korisnickiOdabir;
-}
+void rezIspis(const char* imeDatLinije, const char* imeDatKupci);
+void ispisLinija(const char* imeDat);
+
+LINIJE* procitajLinijeIzDatoteke(const char* imeDatLinije);
+int procitajBrojLinija(const char* imeDatLinije);
+
+void rezervacija(int odabir, const char* imeDatLinije, const char* imeDatKupci);
+void unesiPodatkeKorisnika(KORISNIK* kupac);
+void zapisiPutnikaUDatoteku(KORISNIK* kupac, const char* imeDatKupci);
+void zapisiLinije(LINIJE* vlakovi, int brojLinija, const char* imeDatLinije);
+
+void ispisBrojaSjedala(const char* imeDatLinije);
+KORISNIK* pretrazivanje(const char* imeDatKupci);
+void odjava(const char* imeDatLinije, const char* imeDatKupci);
+int kupacPostoji(KORISNIK* trazeni, KORISNIK* sviKorisnici, int brojKupaca);
+int postojiDat(const char* imeDatKupci);
+KORISNIK* procitajKorisnikeIzDatoteke(const char* imeDatKupci);
+int procitajBrojKorisnika(const char* imeDatKupci);
+
+void obrisi(KORISNIK* kupac, const char* imeDatKupci);
+
+void izlaz();
+#endif // HEADER_H
+
